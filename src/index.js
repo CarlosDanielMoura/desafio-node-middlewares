7b9099a581d75926d74ab46928675404508b1d53
Carlos Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2023-02-14 13:01:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,18 +11,72 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ "error": "Usuário não encontrado." });
+  } else {
+    request.user = user;
+    return next();
+  }
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+    const {user} = request;
+     if(user.todos.length > 10 && !user.pro){
+         return response.status(403).json({message: 'Seu plano é grátis, podendo apenas criar 10 TODO'})
+     }
+
+    return next();
+
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const {id} = request.params;
+  const {username} = request.headers
+
+  if(!validate(id)){
+    return response.status(400).json({error: 'id invalid'});
+  }
+
+  const user = users.find((user)=> user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: 'User not found'});
+  }
+
+  const todo = user.todos.find((todos)=> todos.id === id)
+
+  if(!todo){
+    return response.status(404).json({error: 'TODO not found'});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+    const {id} = request.params;
+    
+    const user = users.find((user)=> user.id === id)
+
+    if(!user){
+      return response.status(404).json({error:'id not found'})
+    }
+
+
+    request.user = user
+
+    return next();
+
+
 }
 
 app.post('/users', (request, response) => {
